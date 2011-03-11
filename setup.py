b692e3dc80d12e7879637f01cadf504dfef6066e
Andoni Morales Alastruey <ylatuya@gmail.com>
2011-03-11 01:36:49 +0100

    Deploy config files in the setup script

--- setup.py
+++ setup.py
@@ -1,6 +1,58 @@
+import os
+import shutil
+import shlex, subprocess
+
 from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+# FIXME: This will only works for installations invoking setup.py but not
+#        for bdist_rpm or bdit_msi, where we should pass a post-install script
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self._install_data_files()
+
+    def _install_data_files(self):
+        ossbuild_dest = os.path.join(os.environ['HOME'], '.ossbuild')
+        patches_dest = os.path.join(ossbuild_dest, 'patches')
+        modulesets_dest = os.path.join(ossbuild_dest, 'modulesets')
+
+        patches_src = os.path.abspath('patches')
+        modulesets_src = os.path.abspath('modulesets')
+
+        try:
+            os.mkdir(ossbuild_dest)
+            os.mkdir(patches_dest)
+            os.mkdir(modulesets_dest)
+        except OSError:
+            # dir exists
+            pass
+
+        patches = os.listdir(patches_src)
+        modulesets = os.listdir(modulesets_src)
+
+        # Copy patches and modulesets to the home directory
+        for patch in patches:
+            shutil.copy2(os.path.join(patches_src, patch),
+                         os.path.join(patches_dest, patch))
+        for moduleset in modulesets:
+            shutil.copy2(os.path.join(modulesets_src, moduleset),
+                         os.path.join(modulesets_dest, moduleset))
+
+        # Save 'ossbuildrc' config file with the module set path
+        try:
+            f=open(os.path.join(ossbuild_dest, 'ossbuildrc'), 'w+')
+            f.write("modulesets_dir = '%s'" % modulesets_dest)
+            f.close()
+        except IOError, e:
+            raise Exception("Could not write 'ossbuilrc': %s", e)
+
+        # ossbuild is now installed, update the modules :)
+        p = subprocess.Popen(shlex.split('ossbuild update_moduleset'))
+
 
 setup(
+    cmdclass={'install': install},
     name = "OSSBuild",
     version = "0.1",
     packages = find_packages() + find_packages('jhbuild'),
